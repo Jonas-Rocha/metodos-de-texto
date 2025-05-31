bbae92dd0df0340dbe977a8b6fddd13d1e4a7693
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-05-31 14:49:11 -0300

    Substituindo e fatiando um texto

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ let value = 12345;
 // se converter para texto funciona
 console.log(String(value).length);
 console.log(value.toString().length);
+
+// Substituindo partes de um texto.
+console.log(
+  message.replace("os fundamentos do Javascript", "Métodos de String")
+); // o conteúdo original de "message" continua o mesmo, porém eu o substituo na hora de mostrar com o .replace
+
+//mostrando o conteúdo original
+console.log(message);
+
+// Extraindo uma parte da string (start, end)
+console.log(message.slice(6, 30));
+
+// Extraindo uma parte da string de trás para frente.
+console.log(message.slice(-11));
+
+let textWithSpace = "   Texto de exemplo   ";
+console.log(textWithSpace.length);
+
+// Remove espaços em branco no início e no final de uma string
+// Ele não remove espaços no meio de uma string pois entende que faz parte da string normal.
+console.log(textWithSpace.trim());
