83a3445e5969e5ece23522c62b43f12ba4315828
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-05-31 13:58:41 -0300

    Texto maiúsculo e minúsculo

---
+++ script.js
@@ -0,0 +1,8 @@
+let message = "Estou estudando os fundamentos do Javascript.";
+
+console.log(message);
+
+// Exibir texto em maiúsculo.
+console.log(message.toUpperCase());
+
+console.log(message.toLowerCase());
